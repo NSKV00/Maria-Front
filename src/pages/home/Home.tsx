5313671f56395c48cde0439b4b27329c0e5b844e
NSKV00 <nicolas.g.g.dsilva@gmail.com>
2025-07-23 20:11:04 -0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,43 @@ import { Header } from "../../components/header/Header"
 export const Home = ()=>{
     const navigate = useNavigate()
 
+    const validateUser = async(token:string)=>{
+        try {
+            const res = await apiController.get("usuarios/retrieve",{
+                headers:{
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            if (res.data){
+                localStorage.setItem("user", JSON.stringify(res.data))
+            }
+        } catch (error){
+            console.log(error,"error")
+            localStorage.removeItem("token")
+            localStorage.removeItem("user")
+             navigate("/login")
+        }
+    }
+    const update=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        console.log("update",e.target.files)
+        const reader = new FileReader()
+        const file = e.target.files![0]
+        reader.readAsText(file)
+        reader.onload=(even)=>{
+            console.log("reader",even.target!.result)
+        }
+        
+    }
+    useEffect(()=>{
+        console.log("use effect")
+        const token = localStorage.getItem("token")
+        if (!token){
+            navigate("/login")
+        }else {
+            validateUser(token)
+        }
+    },[])
+    
     return<>
     <Header />
         <main>
